c22c5dc5853605363f89a6fcd07701eb48abe533
Faskomilo <oscarve.do@outlook.com>
2023-06-18 02:38:53 -0600

    modifying into multiple files

--- main.py
+++ main.py
@@ -11,72 +11,75 @@ def main():
     print("""Which game would you like to play?
 
         Options:
-            1. Write it down: 
-                A game where the game will tell you which syllable to write, and
+            1. Reading combos:
+                A combination of syllabs and logographs (kanjis) will appear and
+                you have to read it and write it in romanji.
+            2. Reading combos without system hint:
+                A combination of syllabs and logographs (kanjis) will appear and
+                you have to read it and write it in romanji.
+            3. Write it down: 
+                A game where the game will tell you which syllable to write on paper, and
                 you have to write it before the next one comes.
-            2. Reading combos:
-                A combination of syllabs and logographs (kanjis) will appear and
-                you have to read it and write it in romanji.
-            3. Read combos without system hint:
-                A combination of syllabs and logographs (kanjis) will appear and
-                you have to read it and write it in romanji.
-        
         """)
-    game_type = input("Type the number of the game you would like to play: ")
+    game_number = input("Type the number of the game you would like to play: ")
 
-    if game_type is None:
+    if game_number is None:
         print("You have to write a number")
         exit()
     try:
-        game_type = int(game_type)
-        if game_type not in [1,2,3]:
+        game_number = int(game_number)
+        if game_number not in [1,2,3]:
             raise ValueError
     except ValueError or Exception:
-        print(f"Incorrect value, given: {game_type} expected 1, 2, 3")
+        print(f"Incorrect value, given: {game_number} expected 1, 2, 3")
         exit()
+    Games(catalogue).choose_game(game_number)
 
-    if game_type == 1:
-        seconds_between_letters = config["system"]["secs_between_letters"]
-        Games.write_it_down(catalogue, seconds_between_letters)
-    elif game_type == 2:
-        Games.reading_combos(catalogue)
-    elif game_type == 3:
-        Games.reading_combos(catalogue, type_hint=False)
+
 
 class Games():
-    @staticmethod
-    def write_it_down(catalogue, seconds_between_letters):
+
+    def __init__(self, catalogue) -> None:
+        self.game_catalogue = {
+            1: self.reading_combos(catalogue),
+            2: self.reading_combos(catalogue, writting_system_hint=False),
+            3: self.write_it_down(catalogue)
+        }
+
+    def choose_game(self, game_number):
+        self.game_catalogue[game_number]
+    
+    def write_it_down(self, catalogue, seconds_between_letters):
         while True:
             chosen_writting_system = choice(list(catalogue.keys()))
-            chosen_syllabe_kanji = choice(catalogue[chosen_writting_system])
-            print(f"{chosen_writting_system}: {chosen_syllabe_kanji}")
+            chosen_syllabe_or_kanji = choice(catalogue[chosen_writting_system])
+            print(f"{chosen_writting_system}: {chosen_syllabe_or_kanji}")
             sleep(seconds_between_letters)
             if chosen_writting_system == "Hiragana":
-                print(wanakana.to_hiragana(chosen_syllabe_kanji))
+                print(wanakana.to_hiragana(chosen_syllabe_or_kanji))
             elif chosen_writting_system == "Katakana":
-                print(wanakana.to_katakana(chosen_syllabe_kanji))
+                print(wanakana.to_katakana(chosen_syllabe_or_kanji))
             elif chosen_writting_system == "Kanji":
                 print("TBI")
             print("----------------------------------")
 
-    @staticmethod
-    def reading_combos(catalogue, type_hint= True):
+    def reading_combos(self, catalogue, writting_system_hint= True):
         while True:
-            chosen_writting_type = choice(list(catalogue.keys()))
+            chosen_writting_system = choice(list(catalogue.keys()))
             romanji_word = ""
             for _ in range(randrange(1,6)):
-                romanji_word += choice(catalogue[chosen_writting_type])
-            if type_hint:
-                type_hint = chosen_writting_type + ": "
+                romanji_word += choice(catalogue[chosen_writting_system])
+            if writting_system_hint:
+                writting_system_hint = chosen_writting_system + ": "
             else:
-                type_hint = ""
+                writting_system_hint = ""
             print(f"Your combo is:")
-            if chosen_writting_type == "hiragana":
-                print(f"{type_hint}", wanakana.to_hiragana(romanji_word))
-            elif chosen_writting_type == "katakana":
-                print(f"{type_hint}", wanakana.to_katakana(romanji_word))
-            elif chosen_writting_type == "kanji":
-                print(f"{type_hint}", "TBI")
+            if chosen_writting_system == "hiragana":
+                print(f"{writting_system_hint}", wanakana.to_hiragana(romanji_word))
+            elif chosen_writting_system == "katakana":
+                print(f"{writting_system_hint}", wanakana.to_katakana(romanji_word))
+            elif chosen_writting_system == "kanji":
+                print(f"{writting_system_hint}", "TBI")
             romanji_input = input("Romanji: ")
             print("")
             if romanji_word.lower() == romanji_input.lower():
@@ -156,7 +159,7 @@ class Catalogue:
         self._create_catalogue_()
 
     def get_catalogue(self):
-        return self._catalogue_()
+        return self._catalogue_
 
     def _create_catalogue_(self):
         if(self._hiragana_latest_ == 'py'
